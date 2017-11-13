373d889f03ef17b407cbb07f3524d05938629ed6
Arshitha <arsh2794@gmail.com>
2017-11-13 01:54:37 -0500

    template matching updated

--- Template_Matching/TemplateMatching.py
+++ Template_Matching/TemplateMatching.py
@@ -17,13 +17,19 @@ def TemplateMatching(src, temp, stepsize): # src: source image, temp: template i
             var_s = 0;
             corr = 0;
             # Calculate the mean and variance of source image pixel values inside window
-            mean_s = np.mean(src[i:i+temp.shape[0], j:j+temp.shape[1]])
-            var_s = np.var(src[i:i+temp.shape[0], j:j+temp.shape[1]])
-            # ------------------ Put your code below ------------------ 
+            roi = src[i:i+temp.shape[0], j:j+temp.shape[1]]
+            mean_s = np.mean(roi)
+            var_s = np.var(roi) 
             
             # Calculate normalized correlation coefficient (NCC) between source and template
             # ------------------ Put your code below ------------------ 
-            corr = ((src[i,j] - mean_s) * (temp[i,j] - mean_t))/(var_s*var_t)
+            prod = 0 
+            for k in range (temp.shape[0]):
+                for l in range (temp.shape[1]):
+                    prod = prod + (roi[k,l] - mean_s) * (temp[k,l] - mean_t)    
+            corr= (1/(temp.shape[0]*temp.shape[1]))* prod /(var_s * var_t)
+
+            #corr = ((src[i,j] - mean_s) * (temp[i,j] - mean_t))/(var_s*var_t)
             if corr > max_corr:
                 max_corr = corr
                 location = [i, j]
@@ -37,10 +43,10 @@ print(location)
 match_img = cv2.cvtColor(source_img, cv2.COLOR_GRAY2RGB)
 
 # Draw a red rectangle on match_img to show the template matching result
-# ------------------ Put your code below ------------------ 
+box = cv2.rectangle(match_img,(location[0],location[1]),(location[0]+temp.shape[1],location[1]+temp.shape[0]),(0,255,0),3) 
 
 # Save the template matching result image (match_img)
-# ------------------ Put your code below ------------------ 
+cv2.imwrite('matchResult.jpg',box) 
 
 
 # Display the template image and the matching result
@@ -48,5 +54,7 @@ cv2.namedWindow('TemplateImage', cv2.WINDOW_NORMAL)
 cv2.namedWindow('MyTemplateMatching', cv2.WINDOW_NORMAL)
 cv2.imshow('TemplateImage', temp)
 cv2.imshow('MyTemplateMatching', match_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite('TemplateImageGen.png', temp)
+cv2.imwrite('MyTemplateMatching.png', match_img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
