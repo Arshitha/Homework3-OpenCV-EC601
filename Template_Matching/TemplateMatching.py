ddac39a44e89a027f321d6bfab36cf89b8f0e9ca
Arshitha <arsh2794@gmail.com>
2017-11-13 02:00:55 -0500

    results of template matching added

--- Template_Matching/TemplateMatching.py
+++ Template_Matching/TemplateMatching.py
@@ -35,26 +35,28 @@ def TemplateMatching(src, temp, stepsize): # src: source image, temp: template i
                 location = [i, j]
     return location
 
-# load source and template images
-source_img = cv2.imread('source_img.jpg',0) # read image in grayscale
-temp = cv2.imread('template_img.jpg',0) # read image in grayscale
-location = TemplateMatching(source_img, temp, 20);
-print(location)
-match_img = cv2.cvtColor(source_img, cv2.COLOR_GRAY2RGB)
+if __name__ == "__main__":
 
-# Draw a red rectangle on match_img to show the template matching result
-box = cv2.rectangle(match_img,(location[0],location[1]),(location[0]+temp.shape[1],location[1]+temp.shape[0]),(0,255,0),3) 
+    # load source and template images
+    source_img = cv2.imread('source_img.jpg',0) # read image in grayscale
+    temp = cv2.imread('template_img.jpg',0) # read image in grayscale
+    location = TemplateMatching(source_img, temp, 20);
+    print(location)
+    match_img = cv2.cvtColor(source_img, cv2.COLOR_GRAY2RGB)
 
-# Save the template matching result image (match_img)
-cv2.imwrite('matchResult.jpg',box) 
+    # Draw a red rectangle on match_img to show the template matching result
+    box = cv2.rectangle(match_img,(location[0],location[1]),(location[0]+temp.shape[1],location[1]+temp.shape[0]),(0,255,0),3) 
+
+    # Save the template matching result image (match_img)
+    cv2.imwrite('matchResult.jpg',box) 
 
 
-# Display the template image and the matching result
-cv2.namedWindow('TemplateImage', cv2.WINDOW_NORMAL)
-cv2.namedWindow('MyTemplateMatching', cv2.WINDOW_NORMAL)
-cv2.imshow('TemplateImage', temp)
-cv2.imshow('MyTemplateMatching', match_img)
-cv2.imwrite('TemplateImageGen.png', temp)
-cv2.imwrite('MyTemplateMatching.png', match_img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+    # Display the template image and the matching result
+    cv2.namedWindow('TemplateImage', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('MyTemplateMatching', cv2.WINDOW_NORMAL)
+    cv2.imshow('TemplateImage', temp)
+    cv2.imshow('MyTemplateMatching', match_img)
+    cv2.imwrite('TemplateImageGen.png', temp)
+    cv2.imwrite('MyTemplateMatching.png', match_img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
